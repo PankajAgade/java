3fcd793332b4e68695c2ec2f665249ab65d16c4c
PankajAgade <pankajagade.pa@gmail.com>
2020-08-30 20:32:58 +0530

    first commit
    Signed-off-by: PankajAgade <pankajagade.pa@gmail.com>

---
+++ src/Sagar/SagarDemo.java
@@ -0,0 +1,11 @@
+package Sagar;
+
+public class SagarDemo {
+	SagarDemo()
+	{	
+		int a=5,b=5,sum;
+		sum=a+b;
+		System.out.println(sum);
+	}
+
+}
---
+++ src/Sagar/SgarDemoMain.java
@@ -0,0 +1,12 @@
+package Sagar;
+
+public class SgarDemoMain {
+
+	public static void main(String[] args) {
+		SagarDemo obj = new SagarDemo();
+		
+		
+		
+	}
+
+}
